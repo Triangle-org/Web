66cacae1ed709135c6485397599a7a8e6d6829f0
localzet <creator@localzet.com>
2024-02-01 13:52:27 +0300

    Update

--- config/log.php
+++ config/log.php
@@ -22,23 +22,7 @@ return [
                     'class' => Monolog\Formatter\LineFormatter::class,
                     'constructor' => [null, 'Y-m-d H:i:s', true],
                 ],
-            ]
-        ],
-    ],
-    'http' => [
-        'handlers' => [
-            [
-                'class' => Monolog\Handler\RotatingFileHandler::class,
-                'constructor' => [
-                    runtime_path() . '/logs/http.log',
-                    7, //$maxFiles
-                    Monolog\Logger::DEBUG,
-                ],
-                'formatter' => [
-                    'class' => Monolog\Formatter\LineFormatter::class,
-                    'constructor' => [null, 'Y-m-d H:i:s', true],
-                ],
-            ]
-        ],
-    ],
+            ],
+        ]
+    ]
 ];
